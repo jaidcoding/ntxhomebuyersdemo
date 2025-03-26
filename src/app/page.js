a490d8890f060702ef7c843237103d7bd29ae2b6
Jaidon Clinton <jaidcoding@gmail.com>
2025-03-25 19:55:59 -0500

    Add InfoBox component to Home page for enhanced information display

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import Info from '../components/InfoBox';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <Hero />
+      <Info />
     </main>
   );
 }
